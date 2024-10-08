460ca36fccb2dbe4867335152362f5629db3d29a
amansharma193 <72331702+amansharma193@users.noreply.github.com>
2024-10-08 12:27:59 +0530

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,20 +1,41 @@
-import { StrictMode } from "react";
+import { StrictMode, Suspense, lazy } from "react";
 import { createRoot } from "react-dom/client";
-import App from "./App.tsx";
+// import App from "./App.tsx";
 import "./index.css";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import CreateTrip from "./create-trip/index.tsx";
+// import CreateTrip from "./create-trip/index.tsx";
 import Header from "./components/custom/Header.tsx";
 import { Toaster } from "@/components/ui/sonner";
 import { GoogleOAuthProvider } from "@react-oauth/google";
-import ViewTrip from "./view-trip/[tripId]/index.tsx";
-import MyTrips from "./my-trips/index.tsx";
+// import ViewTrip from "./view-trip/[tripId]/index.tsx";
+// import MyTrips from "./my-trips/index.tsx";
+
+const MyTrips = lazy(() => import("./my-trips/index.tsx"));
+const ViewTrip = lazy(() => import("./view-trip/[tripId]/index.tsx"));
+const CreateTrip = lazy(() => import("./create-trip/index.tsx"));
+const App = lazy(() => import("./App.tsx"));
 
 const router = createBrowserRouter([
-  { path: "/", element: <App /> },
-  { path: "/create-trip", element: <CreateTrip /> },
-  { path: "/view-trip/:tripId", element: <ViewTrip /> },
-  { path: "/my-trips", element: <MyTrips /> },
+  { path: "/", element: (
+    <Suspense fallback={<div>Loading...</div>}>
+      <App />
+    </Suspense>
+  ) },
+  { path: "/create-trip", element: (
+    <Suspense fallback={<div>Loading...</div>}>
+      <CreateTrip />
+    </Suspense>
+  ) },
+  { path: "/view-trip/:tripId", element: (
+    <Suspense fallback={<div>Loading...</div>}>
+      <ViewTrip />
+    </Suspense>
+  ) },
+  { path: "/my-trips", element: (
+    <Suspense fallback={<div>Loading...</div>}>
+      <MyTrips />
+    </Suspense>
+  ) },
 ]);
 
 createRoot(document.getElementById("root")!).render(
